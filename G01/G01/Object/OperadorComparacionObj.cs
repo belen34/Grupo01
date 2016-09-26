540faa8740295008ca220e8cfd4772e614e4d4f2
anabelen.sarabia <anabelen.sarabia@L041011>
2016-09-26 18:28:40 +0200

    Unido

---
+++ G01/G01/Object/OperadorComparacionObj.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G01.Object
+{
+    public class OperadorComparacionObj
+    {
+        public int operadorComparacionId;
+        public string name;
+
+        public OperadorComparacionObj()
+        {
+
+        }
+
+        public OperadorComparacionObj(int _operadorComparacionId, string _name)
+        {
+            this.operadorComparacionId = _operadorComparacionId;
+            this.name = _name;
+        }
+    }
+}
---
+++ G01/G01/Repository/IOperadorComparacionRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using G01.Object;
+
+namespace G01.Repository
+{
+    public interface IOperadorComparacionRepository
+    {
+        ICollection<OperadorComparacionObj> ListOperadorComparacionObj();
+    }
+}

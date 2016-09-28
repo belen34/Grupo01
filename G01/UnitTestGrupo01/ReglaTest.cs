85b6b457c49714707ed4a05c81d433fc08f1cb9a
anabelen.sarabia <anabelen.sarabia@L041011>
2016-09-28 18:27:10 +0200

    Cambios pruebas regla

--- G01/UnitTestGrupo01/ReglaTest.cs
+++ G01/UnitTestGrupo01/ReglaTest.cs
@@ -36,7 +36,7 @@ namespace UnitTestGrupo01
         [TestMethod]
         public void ReglaTest_GiveRegla()
         {
-            ReglaVO r = this.iReglaController.Create(1, "superior");
+            ReglaVO r = this.iReglaController.Create( "superior");
 
             ReglaVO res = this.iReglaController.Give(r.reglaId);
             Assert.AreEqual(res.reglaId, r.reglaId);
@@ -46,14 +46,14 @@ namespace UnitTestGrupo01
         [TestMethod]
         public void ReglaTest_CrearRegla()
         {
-            ReglaVO r = this.iReglaController.Create(2,"inferior");
+            ReglaVO r = this.iReglaController.Create("inferior");
             Assert.AreEqual(2, r.reglaId);
 
         }
         [TestMethod]
         public void ReglaTest_UpdateRegla()
         {
-            ReglaVO r = this.iReglaController.Create(3, "igual");
+            ReglaVO r = this.iReglaController.Create("igual");
             r.nombre = "igualdad";
             ReglaVO sUpdate = this.iReglaController.UpDate(r);
             Assert.AreEqual(sUpdate.nombre, r.nombre);
@@ -65,7 +65,7 @@ namespace UnitTestGrupo01
         [TestMethod]
         public void ReglaTest_DeleteRegla()
         {
-            ReglaVO r = this.iReglaController.Create(4, "distinto");
+            ReglaVO r = this.iReglaController.Create("distinto");
             ReglaVO sDelete = this.iReglaController.Delete(r.reglaId);
             Assert.AreEqual(sDelete.reglaId, r.reglaId);
             Assert.AreEqual(sDelete.nombre, r.nombre);
